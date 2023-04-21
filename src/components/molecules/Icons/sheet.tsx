2704d5b7aeddb9158e1e49a5708ee3f79db7d86b
4eug <debraheug@gmail.com>
2023-04-21 09:25:55 +0000

    Icons

---
+++ src/components/molecules/Icons/sheet.tsx
@@ -0,0 +1,26 @@
+export const Sheet = () => (
+  <svg
+    width="43"
+    height="50"
+    viewBox="0 0 53 70"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_2_1525)">
+      <path
+        d="M0 4H3.25001V65.7502H0V4ZM6.50002 65.7502V4.00016H52V65.7502H6.50002ZM13 12.125V17H45.5001V12.125H13ZM13 25.1251V30.0001H17.8751V25.1251H13ZM22.7501 25.1251V30.0001H43.8751V25.1251H22.7501ZM13 38.1251V43.0001H17.8751V38.1251H13ZM22.7501 38.1251V43.0001H42.2501V38.1251H22.7501ZM13 51.1251V56.0002H17.8751V51.1251H13ZM22.7501 51.1251V56.0002H45.5001V51.1251H22.7501Z"
+        fill="#FFB600"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_2_1525">
+        <rect
+          width="52"
+          height="69.33"
+          fill="white"
+          transform="translate(0.459961)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+);
